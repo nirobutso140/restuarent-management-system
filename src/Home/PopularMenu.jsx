ceaaa0f75c8b5c7feafaef7eaf128e7bc03a0e61
nirobutso140 <nirobutso140@gmail.com>
2024-04-08 13:15:45 +0600

    create custom hook and display popular menu data using this hook

--- src/Home/PopularMenu.jsx
+++ src/Home/PopularMenu.jsx
@@ -1,23 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import Section_title from '../components/Section_title';
 import Menu from '../Shared/Menu';
+import useMenu from '../Hooks/useMenu';
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([])
-    useEffect(()=>{
-         fetch('menu.json ')
-         .then(res => res.json())
-         .then(data =>{
-            const popularItems = data.filter(item => item.category === "popular")
-            setMenu(popularItems)
-         })
-    }, [])
+    
+    const [menu] = useMenu()
+    const popular = menu.filter(item => item.category === "popular") 
+
     return (
         <>
             <Section_title subheading={"--- Propular Menu ---"} heading={"From Our Menu"}/>
            <div className='grid gap-4 md:grid-cols-2'>
                {
-                 menu.map(item => <Menu items={item} key={item._id}/>)
+                 popular.map(item => <Menu items={item} key={item._id}/>)
                }
            </div>
         </>
