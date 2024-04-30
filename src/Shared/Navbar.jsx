6a79dbe99e09fed0fa13e5290f0c4f076e6d6965
nirobutso140 <nirobutso140@gmail.com>
2024-04-30 12:40:23 +0600

    create login, logout btn to observe user

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
 
 const Navbar = () => {
+    const { user, logOut } = useContext(AuthContext)
+    const handleLogOut = () => {
+        logOut()
+            .then(() => console.log("logout successfully"))
+            .catch(error => console.error(error))
+    }
     const navOptions = <>
         <li><Link to='/'>HOME</Link></li>
         <li><Link to='/menu'>MENU</Link></li>
@@ -19,7 +26,7 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                               {navOptions}
+                            {navOptions}
                         </ul>
                     </div>
                     <a className="btn btn-ghost text-xl">Sabbir's Kichen</a>
@@ -30,7 +37,18 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    {
+
+                        user ?
+                            <>
+                                <span className="mx-3">{user?.displayName}</span>
+                                <button onClick={handleLogOut} className="btn btn-sm btn-secondary">Log out</button>
+                            </>
+                            :
+                            <Link to='/login'>
+                                <button className="btn btn-sm">Log in</button>
+                            </Link>
+                    }
                 </div>
             </div>
         </>
