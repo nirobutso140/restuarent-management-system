2e73463e8d990868a73fbf9230f23d3de7f56fa5
nirobutso140 <nirobutso140@gmail.com>
2024-05-02 12:51:56 +0600

    hide some routes if user is not logged in

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -11,8 +11,11 @@ const Navbar = () => {
     }
     const navOptions = <>
         <li><Link to='/'>HOME</Link></li>
-        <li><Link to='/menu'>MENU</Link></li>
-        <li><Link to='/shop'>OUR SHOP</Link></li>
+        {user && <>
+            <li><Link to='/menu'>MENU</Link></li>
+            <li><Link to='/shop'>OUR SHOP</Link></li>
+        </>
+        }
         <li><Link to='/contact'>CONTACT US</Link></li>
         <li><Link to='/login'>LOGIN</Link></li>
         <li><Link to='/register'>REGISTER</Link></li>
